98dfad773f1a87e1f1924ba162765d2dd23d8f1b
Luciano Bandeira <luciano062@gmail.com>
2017-03-16 16:53:15 -0300

    Display posts from state.

--- src/App.js
+++ src/App.js
@@ -12,11 +12,22 @@ class App extends Component {
 }
 
 class Feed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [
+        {content: 'This is my first post!'},
+        {content: 'This is my second post!'}
+      ]
+    }
+  }
   render() {
+    const posts = this.state.posts.map((post, index) =>
+      <Post key={index} value={post} />
+    );
     return (
       <div className="feed">
-        <Post content="This is my first post!" />
-        <Post content="This is my second post!" />
+        {posts}
       </div>
     )
   }
@@ -26,7 +37,7 @@ class Post extends Component {
   render() {
     return (
       <div className="post">
-        <span>{this.props.content}</span>
+        <span className="content">{this.props.value.content}</span>
       </div>
     )
   }
